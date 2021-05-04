a279dd76847b81e02c74d25d9694a487817365a2
smcmanis <simon.mcmanis@gmail.com>
2021-05-04 13:58:29 +1000

    Make orderservlet get cartItems to calculate their subtotals

--- src/main/java/com/iotbay/shop/controller/OrderServlet.java
+++ src/main/java/com/iotbay/shop/controller/OrderServlet.java
@@ -1,8 +1,12 @@
 package com.iotbay.shop.controller;
 
+import com.iotbay.shop.dao.CartItemDao;
 import com.iotbay.shop.dao.OrderDao;
+import com.iotbay.shop.model.Cart;
+import com.iotbay.shop.model.CartItem;
 import com.iotbay.shop.model.Order;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,12 +21,17 @@ import javax.servlet.http.HttpServletResponse;
 public class OrderServlet extends HttpServlet {
 
     private OrderDao orderDao = new OrderDao();
+    private CartItemDao cartItemDao = new CartItemDao();
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Integer orderId = Integer.parseInt(request.getParameter("orderId"));
         Order order = orderDao.getOrderByOrderId(orderId);
         request.setAttribute("order", order);
-
+            
+        Cart cart = order.getCart();
+        for (CartItem item : cart.getCartItems()) {
+            item.setSubtotal(cartItemDao.calculateSubtotal(item));
+        }
         RequestDispatcher dispatcher = request.getRequestDispatcher("/order.jsp");
         dispatcher.forward(request, response);
     }
