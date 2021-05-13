e60b9be865b53f93111d7eef21ef943bfa4ab1ca
smcmanis <simon.mcmanis@gmail.com>
2021-05-13 19:10:40 +1000

    Route index to main.jsp

---
+++ src/main/java/com/iotbay/shop/controller/MainServlet.java
@@ -0,0 +1,29 @@
+package com.iotbay.shop.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(
+        name = "MainServlet",
+        urlPatterns = {""})
+public class MainServlet extends HttpServlet {
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("main.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
