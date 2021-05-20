269758d269e5b7d60462701e1388e5a21fc1f1c0
smcmanis <simon.mcmanis@gmail.com>
2021-05-20 18:35:58 +1000

    Add basic product class

---
+++ webshop/src/main/java/com/iotbay/webshop/model/Product.java
@@ -0,0 +1,40 @@
+package com.iotbay.webshop.model;
+
+public class Product {
+
+    private String productId;
+
+    private String productName;
+
+    private double productPrice;
+
+    private int stockLevel;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getStockLevel() {
+        return stockLevel;
+    }
+
+    public void setStockLevel(int stockLevel) {
+        this.stockLevel = stockLevel;
+    }
+}
